4fe0fefc2c9e5cc7f168f0cdecfd4a86a70390d8
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-03-17 21:26:25 +0700

    33. Updating Widget Data (Or: Using StatelessWidget Incorrectly)

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -3,10 +3,23 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var indexQuestion = 0;
+  var questions = [
+    "What\'s your favorite color?",
+    "What\'s your favorite animal?",
+    "What\'s your favorite flower?"
+  ];
 
   void answerQuestion() {
-    print("Chosen answer!");
+    if(indexQuestion < 2) {
+      indexQuestion = indexQuestion + 1;
+      print(indexQuestion);
+    } else {
+      indexQuestion = 0;
+      print(indexQuestion);
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,21 +29,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text("This is Question!"),
+            Text(questions[indexQuestion]),
             RaisedButton(
               child: Text("Answer 1."),
               onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text("Answer 2."),
-              onPressed: () => print("Chosen answer 2!"),
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text("Answer 3."),
-              onPressed: () {
-                //... do something
-                print("Chosen answer 3!");
-              },
+              onPressed: answerQuestion,
             ),
           ],
         ),
