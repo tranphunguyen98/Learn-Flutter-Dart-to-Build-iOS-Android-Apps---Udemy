7e1093c41426e9eb3e174c804bfd97a12912b98e
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-03-20 14:14:20 +0700

    95. Adding Images to the App

--- personal_expenses/lib/widgets/list_transaction.dart
+++ personal_expenses/lib/widgets/list_transaction.dart
@@ -11,39 +11,57 @@ class ListTransaction extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (buildContext, index) {
-          return Card(
-            child: Row(children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(4)),
-                    border: Border.all(color: Theme.of(context).primaryColor, width: 2)),
-                padding: EdgeInsets.all(8),
-                child: Text(
-                  "\$${_transactions[index].amount.toStringAsFixed(2)}",
-                  style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                ),
-              ),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      child: _transactions.isEmpty
+          ? Column(
+              children: <Widget>[
                 Text(
-                  _transactions[index].title,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  "No transaction added yet!",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
-                Text(
-                  DateFormat.yMMMd().format(_transactions[index].date),
-                  style: TextStyle(color: Colors.grey),
-                )
-              ]),
-            ]),
-          );
-        },
-        itemCount: _transactions.length,
-      ),
+                SizedBox(height: 12,),
+                Container(
+                  height: 200,
+                  child: Image.asset("assets/images/sleep.png",fit: BoxFit.cover,)),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (buildContext, index) {
+                return Card(
+                  child: Row(children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(4)),
+                          border: Border.all(
+                              color: Theme.of(context).primaryColor, width: 2)),
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        "\$${_transactions[index].amount.toStringAsFixed(2)}",
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            _transactions[index].title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat.yMMMd()
+                                .format(_transactions[index].date),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ]),
+                  ]),
+                );
+              },
+              itemCount: _transactions.length,
+            ),
     );
   }
 }
