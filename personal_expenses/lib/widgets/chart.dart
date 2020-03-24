2be001dcb4aa9ba46ca97c96c45c6b76ae9d78a0
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-03-24 21:11:45 +0700

    119. Using the LayoutBuilder Widget

--- personal_expenses/lib/widgets/chart.dart
+++ personal_expenses/lib/widgets/chart.dart
@@ -39,28 +39,25 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height *0.25,
-      child: Card(
-          elevation: 6,
-          margin: EdgeInsets.all(20),
-          child: Padding(
-            padding: EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: groupedTransactionValues.map((data) {
-                return Flexible(
-                  fit: FlexFit.tight,
-                  child: ChartBar(
-                      data['day'],
-                      data['amount'],
-                      totalSpending == 0.0
-                          ? 0.0
-                          : (data['amount'] as double) / totalSpending),
-                );
-              }).toList(),
-            ),
-          )),
-    );
+    return Card(
+        elevation: 6,
+        margin: EdgeInsets.all(20),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransactionValues.map((data) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartBar(
+                    data['day'],
+                    data['amount'],
+                    totalSpending == 0.0
+                        ? 0.0
+                        : (data['amount'] as double) / totalSpending),
+              );
+            }).toList(),
+          ),
+        ));
   }
 }
