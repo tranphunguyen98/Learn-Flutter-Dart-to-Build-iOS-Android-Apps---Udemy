7f54014a3aea1c3df22d84ab4102171228577415
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-03-26 14:10:22 +0700

    156. Creating a Grid & Working with Linear Gradients

---
+++ meals_app/lib/widgets/category_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  CategoryItem(this.category);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Text(category.title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.7),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+    );
+  }
+}
