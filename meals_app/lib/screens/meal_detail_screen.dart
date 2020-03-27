2086b410551089dfd0197c04649f078bfc260e69
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-03-27 16:06:26 +0700

    170. Adding a TabBar to the Appbar

--- meals_app/lib/screens/meal_detail_screen.dart
+++ meals_app/lib/screens/meal_detail_screen.dart
@@ -5,6 +5,29 @@ class MealDetailScreen extends StatelessWidget {
   static const routeName = "/meal-detail";
   const MealDetailScreen({Key key}) : super(key: key);
 
+  Widget _buildSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 12),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
+  Widget _buildContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: Colors.grey),
+      ),
+      height: 150,
+      width: 300,
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context).settings.arguments as Meal;
@@ -12,9 +35,49 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Container(
-        child: Center(
-          child: Text("Detail!"),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(bottom: 24),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  meal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              _buildSectionTitle(context, "Ingredients"),
+              _buildContainer(
+                ListView.builder(
+                  itemBuilder: (context, index) => Card(
+                    color: Theme.of(context).accentColor,
+                    child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                        child: Text(meal.ingredients[index])),
+                  ),
+                  itemCount: meal.ingredients.length,
+                ),
+              ),
+              _buildSectionTitle(context, "Steps"),
+              _buildContainer(ListView.builder(
+                itemBuilder: (context, index) => Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text("# ${index + 1}"),
+                      ),
+                      title: Text(meal.steps[index]),
+                    ),
+                    Divider(),
+                  ],
+                ),
+                itemCount: meal.steps.length,
+              )),
+            ],
+          ),
         ),
       ),
     );
