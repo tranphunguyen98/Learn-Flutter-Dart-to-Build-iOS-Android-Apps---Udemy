cfa1896a35c2080edda7fdd23aa1b8b2050ae4a5
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-03-27 14:43:04 +0700

    165. Displaying Recipe Items & Using Network Images

--- meals_app/lib/widgets/category_meals_screen.dart
+++ meals_app/lib/widgets/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/dummy_data/dummy_data.dart';
 import 'package:meals_app/models/category.dart';
+import 'package:meals_app/widgets/meal_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = "/category-meals";
@@ -21,7 +22,7 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
           itemBuilder: (context, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(categoryMeals[index]);
           },
           itemCount: categoryMeals.length),
     );
---
+++ meals_app/lib/widgets/meal_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meals.dart';
+
+class MealItem extends StatelessWidget {
+  final Meal meal;
+
+  MealItem(this.meal);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        elevation: 4,
+        margin: EdgeInsets.all(8),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
