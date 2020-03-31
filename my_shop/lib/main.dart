8e961b908facf4d0858941b81ac641970591ef2f
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-03-31 10:30:13 +0700

    218. Using Forms & Working with Form Inputs

--- my_shop/lib/main.dart
+++ my_shop/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:my_shop/providers/cart.dart';
 import 'package:my_shop/providers/orders.dart';
 import 'package:my_shop/providers/products.dart';
 import 'package:my_shop/screens/cart_screen.dart';
+import 'package:my_shop/screens/edit_product_screen.dart';
 import 'package:my_shop/screens/orders_screen.dart';
 import 'package:my_shop/screens/user_product_screen.dart';
 
@@ -42,6 +43,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (_) => CartScreen(),
           OrdersScreen.routeName: (_) => OrdersScreen(),
           UserProductScreen.routeName: (_) => UserProductScreen(),
+          EditProductScreen.routeName: (_) => EditProductScreen(),
         },
       ),
     );
