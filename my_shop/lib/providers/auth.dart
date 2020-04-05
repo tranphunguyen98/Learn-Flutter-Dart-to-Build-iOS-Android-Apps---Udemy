09c63fe177517d34302e3e6b1082c3707f562547
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-04-05 09:13:54 +0700

    270. Adding a Logout Functionality

--- my_shop/lib/providers/auth.dart
+++ my_shop/lib/providers/auth.dart
@@ -67,4 +67,11 @@ class Auth with ChangeNotifier {
   Future<void> signup(String email, String password) async {
     return _authenticate(email, password, "signUp");
   }
+
+  void logout() {
+    _token = null;
+    _expiryDate = null;
+    _userId = null;
+    notifyListeners();
+  }
 }
