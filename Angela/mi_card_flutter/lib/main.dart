eb3ecdd3c35620069e209eb15245d4d38d51c551
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-04-06 21:34:52 +0700

    4. How to use Column & Row Widgets for Layout

--- Angela/mi_card_flutter/lib/main.dart
+++ Angela/mi_card_flutter/lib/main.dart
@@ -11,9 +11,32 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.yellow,
         body: SafeArea(
-          child: Container(
-            color: Colors.purple,
-            child: Text("Heleo"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                color: Colors.purple,
+                width: 100,
+                height: 100,
+                child: Text("1"),
+              ),
+              SizedBox(
+                height: 150,
+              ),
+              Container(
+                color: Colors.red,
+                width: 100,
+                height: 100,
+                child: Text("2"),
+              ),
+              Container(
+                color: Colors.green,
+                width: 100,
+                height: 100,
+                child: Text("3"),
+              ),
+            ],
           ),
         ),
       ),
