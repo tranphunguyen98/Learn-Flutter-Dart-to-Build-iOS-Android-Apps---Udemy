167c82515456560e9dc4bfb56794a98be166c53d
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-04-08 14:13:59 +0700

    4. Displaying the Questions

--- Angela/quizzler/lib/main.dart
+++ Angela/quizzler/lib/main.dart
@@ -30,111 +30,89 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  final questions = DummyData.questions;
   final List<bool> scoreKeeper = [];
+  final questions = DummyData.questions;
   int indexQuestion = 0;
-
-  int get numberCorrect {
-    int count = 0;
-    scoreKeeper.forEach((check) {
-      if (check) {
-        count += 1;
-      }
-    });
-    return count;
-  }
 
   @override
   Widget build(BuildContext context) {
-    return indexQuestion == questions.length - 1
-        ? Center(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Center(
             child: Text(
-              '$numberCorrect Correct',
-              style: TextStyle(color: Colors.white, fontSize: 30),
+              questions[indexQuestion].content,
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          )
-        : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Expanded(
-                flex: 5,
-                child: Center(
-                  child: Text(
-                    questions[indexQuestion].content,
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            child: Text(
+              'True',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              Expanded(
-                child: FlatButton(
-                  child: Text(
-                    'True',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  color: Colors.green,
-                  onPressed: () {
-                    setState(() {
-                      if (questions[indexQuestion].answer == true) {
-                        scoreKeeper.add(true);
-                      } else {
-                        scoreKeeper.add(false);
-                      }
-                      if (indexQuestion < questions.length - 1) {
-                        indexQuestion += 1;
-                      } else {
-                        print('done');
-                      }
-                    });
-                  },
-                ),
+            ),
+            color: Colors.green,
+            onPressed: () {
+              setState(() {
+                indexQuestion++;
+              });
+            },
+          ),
+        ),
+        SizedBox(
+          height: 24.0,
+        ),
+        Expanded(
+          child: FlatButton(
+            child: Text(
+              'False',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(
-                height: 24.0,
-              ),
-              Expanded(
-                child: FlatButton(
-                  child: Text(
-                    'False',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  color: Colors.red,
-                  onPressed: () {},
-                ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              Row(
-                  children: scoreKeeper.map((check) {
-                if (check) {
-                  return Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  );
-                } else {
-                  return Icon(
-                    Icons.close,
-                    color: Colors.red,
-                  );
-                }
-              }).toList()),
-              SizedBox(
-                height: 8.0,
-              ),
-            ],
-          );
+            ),
+            color: Colors.red,
+            onPressed: () {
+              setState(() {
+                indexQuestion++;
+              });
+            },
+          ),
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        Row(
+            children: scoreKeeper.map((check) {
+          if (check) {
+            return Icon(
+              Icons.check,
+              color: Colors.green,
+            );
+          } else {
+            return Icon(
+              Icons.close,
+              color: Colors.red,
+            );
+          }
+        }).toList()),
+        SizedBox(
+          height: 8.0,
+        ),
+      ],
+    );
   }
 }
