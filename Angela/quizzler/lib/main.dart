1d3f4e664accaaf7eed39a8fa6a4bb1138ca41d5
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-04-08 13:52:25 +0700

    On my own - done

--- Angela/quizzler/lib/main.dart
+++ Angela/quizzler/lib/main.dart
@@ -34,90 +34,107 @@ class _QuizPageState extends State<QuizPage> {
   final List<bool> scoreKeeper = [];
   int indexQuestion = 0;
 
+  int get numberCorrect {
+    int count = 0;
+    scoreKeeper.forEach((check) {
+      if (check) {
+        count += 1;
+      }
+    });
+    return count;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Center(
+    return indexQuestion == questions.length - 1
+        ? Center(
             child: Text(
-              questions[indexQuestion].content,
-              style: TextStyle(
-                fontSize: 25,
-                color: Colors.white,
-              ),
-              textAlign: TextAlign.center,
+              '$numberCorrect Correct',
+              style: TextStyle(color: Colors.white, fontSize: 30),
             ),
-          ),
-        ),
-        Expanded(
-          child: FlatButton(
-            child: Text(
-              'True',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+          )
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 5,
+                child: Center(
+                  child: Text(
+                    questions[indexQuestion].content,
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               ),
-            ),
-            color: Colors.green,
-            onPressed: () {
-              setState(() {
-                if (questions[indexQuestion].answer == true) {
-                  scoreKeeper.add(true);
+              Expanded(
+                child: FlatButton(
+                  child: Text(
+                    'True',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  color: Colors.green,
+                  onPressed: () {
+                    setState(() {
+                      if (questions[indexQuestion].answer == true) {
+                        scoreKeeper.add(true);
+                      } else {
+                        scoreKeeper.add(false);
+                      }
+                      if (indexQuestion < questions.length - 1) {
+                        indexQuestion += 1;
+                      } else {
+                        print('done');
+                      }
+                    });
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              Expanded(
+                child: FlatButton(
+                  child: Text(
+                    'False',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  color: Colors.red,
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Row(
+                  children: scoreKeeper.map((check) {
+                if (check) {
+                  return Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  );
                 } else {
-                  scoreKeeper.add(false);
+                  return Icon(
+                    Icons.close,
+                    color: Colors.red,
+                  );
                 }
-                if (indexQuestion < questions.length - 1) {
-                  indexQuestion += 1;
-                } else {
-                  print('done');
-                }
-              });
-            },
-          ),
-        ),
-        SizedBox(
-          height: 24.0,
-        ),
-        Expanded(
-          child: FlatButton(
-            child: Text(
-              'False',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+              }).toList()),
+              SizedBox(
+                height: 8.0,
               ),
-            ),
-            color: Colors.red,
-            onPressed: () {},
-          ),
-        ),
-        SizedBox(
-          height: 8.0,
-        ),
-        Row(
-            children: scoreKeeper.map((check) {
-          if (check) {
-            return Icon(
-              Icons.check,
-              color: Colors.green,
-            );
-          } else {
-            return Icon(
-              Icons.close,
-              color: Colors.red,
-            );
-          }
-        }).toList()),
-        SizedBox(
-          height: 8.0,
-        ),
-      ],
-    );
+            ],
+          );
   }
 }
