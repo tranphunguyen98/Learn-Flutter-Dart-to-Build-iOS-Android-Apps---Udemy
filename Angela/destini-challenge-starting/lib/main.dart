8b40e823147cbf0bf1dc0281a4034c1b85562592
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-04-09 22:03:30 +0700

    Step 8,9,10: Display the Story in the App

--- Angela/destini-challenge-starting/lib/main.dart
+++ Angela/destini-challenge-starting/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:destini_challenge_starting/story_brain.dart';
 import 'package:flutter/material.dart';
 
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
@@ -13,13 +14,13 @@ class Destini extends StatelessWidget {
   }
 }
 
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
-
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
 }
 
 class _StoryPageState extends State<StoryPage> {
+  final storyBrain = StoryBrain();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,8 +41,7 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
+                    storyBrain.getStory().first.storyTitle,
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
