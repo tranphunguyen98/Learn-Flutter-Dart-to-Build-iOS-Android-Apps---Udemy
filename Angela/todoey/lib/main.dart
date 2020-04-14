a75f49dff7e37b7be927513610bb71f0353b4414
tranphunguyen98 <tranphunguyen.fit@gmail.com>
2020-04-14 14:07:40 +0700

    2. Designing the To-Do List App - code along

--- Angela/todoey/lib/main.dart
+++ Angela/todoey/lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/screens/task_screen.dart';
 
-void main() => runApp(TaskScreen());
+void main() => runApp(Todoey());
+
+class Todoey extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(primaryColor: Colors.lightBlueAccent),
+      home: TaskScreen(),
+    );
+  }
+}
--- Angela/todoey/lib/screens/task_screen.dart
+++ Angela/todoey/lib/screens/task_screen.dart
@@ -3,92 +3,97 @@ import 'package:flutter/material.dart';
 class TaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light().copyWith(primaryColor: Colors.lightBlueAccent),
-      home: Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
         ),
-        body: Column(
+        onPressed: () {},
+      ),
+      body: SafeArea(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0, left: 24.0, bottom: 20),
-              child: CircleAvatar(
-                radius: 30.0,
-                backgroundColor: Colors.white,
-                child: Icon(
-                  Icons.list,
-                  color: Theme.of(context).primaryColor,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 24.0),
-              child: Text(
-                'TODOEY',
-                style: TextStyle(
-                  fontSize: 28.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 24.0, top: 8.0),
-              child: Text(
-                '12 Task',
-                style: TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.white,
-                ),
+            Container(
+              padding: const EdgeInsets.only(top: 24.0, left: 24.0, bottom: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 30.0,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.list,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 18.0,
+                  ),
+                  Text(
+                    'TODOEY',
+                    style: TextStyle(
+                      fontSize: 28.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Text(
+                    '12 Task',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ),
             Expanded(
               child: Container(
-                width: double.infinity,
                 margin: EdgeInsets.only(
                   top: 20.0,
                 ),
-                child: Material(
+                decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(30),
                     topRight: Radius.circular(30),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 24.0, right: 24.0),
-                    child: ListView(
-                      children: <Widget>[
-                        ListTile(
-                          leading: Text('This is a task'),
-                          trailing: Checkbox(
-                            onChanged: (value) {},
-                            value: false,
-                          ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+                  child: ListView(
+                    children: <Widget>[
+                      ListTile(
+                        leading: Text('This is a task'),
+                        trailing: Checkbox(
+                          onChanged: (value) {},
+                          value: false,
                         ),
-                        ListTile(
-                          leading: Text('This is a task'),
-                          trailing: Checkbox(
-                            onChanged: (value) {},
-                            value: false,
-                          ),
+                      ),
+                      ListTile(
+                        leading: Text('This is a task'),
+                        trailing: Checkbox(
+                          onChanged: (value) {},
+                          value: false,
                         ),
-                        ListTile(
-                          leading: Text('This is a task'),
-                          trailing: Checkbox(
-                            onChanged: (value) {},
-                            value: true,
-                          ),
+                      ),
+                      ListTile(
+                        leading: Text('This is a task'),
+                        trailing: Checkbox(
+                          onChanged: (value) {},
+                          value: true,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
